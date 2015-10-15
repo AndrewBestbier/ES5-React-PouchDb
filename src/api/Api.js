1c92bac1fd77dfb71b4c58eef42d1934f0fd29d9
Garren Smith <garren.smith@gmail.com>
2015-10-15 11:06:14 +0200

    some notes

--- src/api/Api.js
+++ src/api/Api.js
@@ -21,6 +21,10 @@ var coloursArray = [{
 var Api = {
   saveSurvey: function(currentlyUsing, interestedUsing, usingES6, yearsExperience) {
 
+    //GN: There needs to be some error checking here to make sure this is called correctly
+    //Dates are not great id's what happens later if someone saves a survey at the exact same time. We will get a collision.
+    // Rather let PouchDB autogenerate an _id and have a date field.
+    
     var submission = {
       _id: new Date().toISOString(),
       currentlyUsing: currentlyUsing,
@@ -41,6 +45,15 @@ var Api = {
 
   getSurveyResults: function() {
 
+    //GN You don't need this new Promise here because allDocs returns a promise.
+    //So instead you can do something like this
+    /*
+      return db.allDocs()
+        .then(
+        //Process results and return them
+      );
+
+    */
     return new Promise(function(resolve, reject) {
 
       db.allDocs({
@@ -48,6 +61,23 @@ var Api = {
       }).then(function(result) {
 
         //Reducing the array of result objects into a single object in the required format of Chart.js
+
+        //GN: Instead of having comments here. Rather break all of this into smaller methods that you can call.
+        // so the only thing that should be in this `then` function is a another function called convertResultToChartFormat
+        // then you can decide how to break this up further. But this is looking much better than before.
+        // what is also nice with smaller functions is that they are much easier to test in isolation. You can give the `convertResultsToChartFormat`
+        //some dummy data and make sure it converts correctly. It doesn't matter where the data comes from. So you then are decoupling the conversion away from PouchDB.
+
+
+        //You are not using reduce correctly here. The idea around reduce is that you iterate over something to build something new
+        //What is happening here is that you are iterating over rows. But then your reduce returns a resolved promise which doesn't make sense
+
+        //Lets break down what you want to do here and then you can rething this looping.
+        //Loop over each row in the all docs - forEach
+        //Sum each survey answer - forEach
+        //This is an optional step but might make it easier. Convert the summed survey results into the format for the chart.js
+
+        //Use forEach. Write some tests for this like I've mentioned above and then we can iterate from their.
         return _.reduce(result.rows, function(object, row) {
 
           //Looping through the keys of 'currentlyUsing', 'interestedUsing' etc
@@ -59,6 +89,7 @@ var Api = {
             });
 
             //If the answer does not exist in this category, then it is pushed. If it does exist, it's value is incremented.
+            //GN: This can be its own function `createResultsObject`
             if (!result) {
               //The colours are defined at the top of this module. If these colours are not defined, Chart.js does not function.
               resultsObject[key].push({
